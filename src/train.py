c6ab7fe7ac212e1fe576905ec1d73cf898ccf760
ArneRobben <arnerobben@gmail.com>
2025-01-27 15:07:09 +0000

    config incorporation in training

--- src/train.py
+++ src/train.py
@@ -1,6 +1,6 @@
+import argparse
 import json
 import os
-import sys
 import wandb
 import yaml
 
@@ -11,16 +11,18 @@ import torch.nn as nn
 from typing import Optional
 from sklearn.metrics import f1_score, precision_score, recall_score
 from transformers import (Trainer, TrainingArguments, AutoTokenizer,
-                          DataCollatorWithPadding, AutoModelForSequenceClassification, RobertaForSequenceClassification)
+                          DataCollatorWithPadding, AutoModelForSequenceClassification)
 
+from transformers import (AlbertTokenizer, AlbertForSequenceClassification )
+from transformers import (ModernBertForSequenceClassification, AutoConfig)
 
-def load_yaml_config():
+def load_yaml_config(config_path:str):
     """ Load yaml configuration file.
 
     Returns:
         dict: configuration dictionary
     """
-    with open('../config/train_config.yaml', 'r') as f:
+    with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
     return config
 
@@ -30,6 +32,8 @@ def init_device():
     Returns: 
         str: device to use for training
     """
+    torch.cuda.empty_cache()
+    torch.set_float32_matmul_precision('high')
     if torch.backends.mps.is_built():
         return "mps"
     elif torch.cuda.is_available():
@@ -53,7 +57,7 @@ class HRCSDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.labels)
 
-def train(train_data_path, test_data_path, model_path, config):
+def train(train_data_path, test_data_path, model_path, config, RA_value_counts, class_weighting):
     """
     Finetune a model from the config for the UKHRA data.
 
@@ -71,6 +75,8 @@ def train(train_data_path, test_data_path, model_path, config):
     test_data = pd.read_parquet(test_data_path)
 
     tokenizer = AutoTokenizer.from_pretrained(config['training_settings']['model'])
+    # tokenizer = AlbertTokenizer.from_pretrained(config['training_settings']['model'])
+    
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     train_encoding = tokenizer(train_data['text'].tolist(), truncation=True, padding=True)
@@ -90,6 +96,10 @@ def train(train_data_path, test_data_path, model_path, config):
     # initialize model
     model = AutoModelForSequenceClassification.from_pretrained(config['training_settings']['model'], num_labels=num_labels, problem_type="multi_label_classification")
     # model = RobertaForSequenceClassification.from_pretrained(config['training_settings']['model'], num_labels=num_labels, problem_type="multi_label_classification")
+    # model = AlbertForSequenceClassification.from_pretrained(config['training_settings']['model'], num_labels=num_labels, problem_type="multi_label_classification")
+
+    # model_config = AutoConfig.from_pretrained(config['training_settings']['model'])
+    # model = ModernBertForSequenceClassification.from_pretrained(config['training_settings']['model'], num_labels=num_labels, problem_type="multi_label_classification", reference_compile=False)
     model.to(device)
 
     # initialize training arguments
@@ -100,12 +110,10 @@ def train(train_data_path, test_data_path, model_path, config):
         per_device_eval_batch_size=config['training_settings']['per_device_eval_batch_size'],
         weight_decay=config['training_settings']['weight_decay'],
         report_to=config['training_settings']["report_to"],
+        save_strategy=config['training_settings']['save_strategy'],
         output_dir=model_path,
     )
 
-    # pull in label names and value counts
-    with open(RA_value_counts_path, 'r') as f:
-        RA_value_counts = json.load(f)
     # sort RA_value_counts by key
     RA_value_counts = {k: v for k, v in sorted(RA_value_counts.items(), key=lambda item: item[0])}
     RA_values = list(RA_value_counts.values())
@@ -114,7 +122,6 @@ def train(train_data_path, test_data_path, model_path, config):
 
     # set class weights
     class_weights = torch.tensor(RA_values, dtype=torch.float32).to(device)
-
 
     class WeightedTrainer(Trainer):
         def __init__(self, *args, class_weights: Optional[torch.FloatTensor] = None, **kwargs):
@@ -137,16 +144,26 @@ def train(train_data_path, test_data_path, model_path, config):
 
             return (loss, outputs) if return_outputs else loss
     
-    # initialize trainer
-    trainer = WeightedTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=test_dataset,
-        data_collator=data_collator,
-        compute_metrics=compute_metrics,
-        class_weights=class_weights
-    )
+    # initialize trainer depending on class weighting option
+    if class_weighting:
+        trainer = WeightedTrainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=test_dataset,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics,
+            class_weights=class_weights
+        )
+    else:
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=test_dataset,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics,
+        )
 
     # train and evaluate
     trainer.train()
@@ -159,6 +176,13 @@ def train(train_data_path, test_data_path, model_path, config):
     return metrics
 
 def compute_metrics(eval_pred):
+    """ Compute evaluation metrics to be used in the Trainer.
+
+    Args: eval_pred (tuple): Tuple containing logits and labels.
+
+    Returns:
+        dict: Evaluation metrics (f1, f1_macro, f1_micro, precision, recall)
+    """
     logits, labels = eval_pred
     # apply sigmoid to logits
     logits = torch.sigmoid(torch.tensor(logits)).cpu().detach().numpy()
@@ -168,9 +192,16 @@ def compute_metrics(eval_pred):
     f1 = f1_score(labels, predictions, average=None)
     precision = precision_score(labels, predictions, average=None)
     recall = recall_score(labels, predictions, average=None)
+    print({"f1": f1, "f1_macro": f1_macro, "f1_micro": f1_micro, "precision": precision, "recall": recall})
     return {"f1": f1, "f1_macro": f1_macro, "f1_micro": f1_micro, "precision": precision, "recall": recall}
 
 def plot_metrics(metrics, RA_value_counts):
+    """ Plot evaluation metrics and value counts in wandb
+    
+    Args:
+        metrics (dict): Evaluation metrics.
+        RA_value_counts (dict): Mapping of label to count in the dataset
+    """
     # plot f1 per label
     f1 = metrics['eval_f1']
     precision = metrics['eval_precision']
@@ -188,30 +219,43 @@ def plot_metrics(metrics, RA_value_counts):
 
 
 if __name__ == "__main__":
-    sys.path.append('../data')
-    train_path = '../data/preprocessed/train.parquet'
-    test_path = '../data/preprocessed/test.parquet'
-    RA_value_counts_path = '../data/preprocessed/RA_value_counts.json'
-    label_names_path = '../data/label_names/ukhra_ra.jsonl'
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config-path', type=str, default='config/train_config.yaml')
+    parser.add_argument('--train-path', type=str, default='data/preprocessed/train.parquet')
+    parser.add_argument('--test-path', type=str, default='data/preprocessed/test.parquet')
+    parser.add_argument('--RA-value-counts-path', type=str, default='data/preprocessed/RA_value_counts.json')
+    parser.add_argument('--label-names-path', type=str, default='data/label_names/ukhra_ra.jsonl')
+    parser.add_argument('--model-dir', type=str, default='data/model')
+    args = parser.parse_args()
 
-    config = load_yaml_config()
+    config = load_yaml_config(args.config_path)
     wandb.init(project=config['wandb_settings']['project_name'])
     timestamp = pd.Timestamp.now().strftime("%Y%m%d%H%M%S")
     model_name = config['training_settings']['model']
-    model_path = f'../data/model/{model_name}_{timestamp}'
+    model_path = f'{args.model_dir}/{model_name}_{timestamp}'
+    wandb.log({"model_path": model_path})
     # check if model path exists
     if not os.path.exists(model_path):
         os.makedirs(model_path)
 
     # pull in label names and value counts
-    with open(RA_value_counts_path, 'r') as f:
+    with open(args.RA_value_counts_path, 'r') as f:
         RA_value_counts = json.load(f)
 
-    with open(label_names_path, 'r') as f:
+    with open(args.label_names_path, 'r') as f:
         label_names = {k: v for line in f for k, v in json.loads(line).items()}
 
     RA_value_counts = {label+"-"+label_names[label]: count for label, count in RA_value_counts.items()}
     print(RA_value_counts)
 
-    metrics = train(train_data_path=train_path, test_data_path=test_path, model_path=model_path, config=config)
+    class_weighting = config['training_settings']['class_weighting']
+    metrics = train(
+        train_data_path=args.train_path, 
+        test_data_path=args.test_path, 
+        model_path=args.model_dir, 
+        config=config, 
+        RA_value_counts=RA_value_counts, 
+        class_weighting=class_weighting
+        )
     plot_metrics(metrics, RA_value_counts)
