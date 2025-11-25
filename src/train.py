59fe652b04f16fa597c60a7ef724763f1fdddb5d
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2025-11-25 00:00:11 +0000

    Clean-up

--- src/train.py
+++ src/train.py
@@ -175,7 +175,7 @@ def train(train_data, test_data, model_path, config, class_counts, class_weighti
             eval_dataset=test_dataset,
             data_collator=data_collator,
             compute_metrics=compute_metrics,
-            class_weights=class_weights
+            class_weights=class_weights,
         )
     else:
         trainer = Trainer(
@@ -184,7 +184,7 @@ def train(train_data, test_data, model_path, config, class_counts, class_weighti
             train_dataset=train_dataset,
             eval_dataset=test_dataset,
             data_collator=data_collator,
-            compute_metrics=compute_metrics
+            compute_metrics=compute_metrics,
         )
 
     # train and evaluate
